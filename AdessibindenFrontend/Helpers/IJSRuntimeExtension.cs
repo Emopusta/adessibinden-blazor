a57e5c93dcc035c05b9e3d4a81ded79e1c8fa589
Emopusta <emreduman225@hotmail.com>
2024-01-02 11:40:27 +0300

    bugfix

--- AdessibindenFrontend/Helpers/IJSRuntimeExtension.cs
+++ AdessibindenFrontend/Helpers/IJSRuntimeExtension.cs
@@ -11,7 +11,7 @@ namespace AdessibindenFrontend.Helpers
 
         public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
         {
-            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
         }
     }
 }
