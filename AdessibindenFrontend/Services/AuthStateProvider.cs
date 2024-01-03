43d462d3915fad10965f541fd14063e4502a90d8
Emopusta <emreduman225@hotmail.com>
2024-01-03 18:08:55 +0300

    added refresh token logic v1

--- AdessibindenFrontend/Services/AuthStateProvider.cs
+++ AdessibindenFrontend/Services/AuthStateProvider.cs
@@ -1,10 +1,16 @@
 ﻿using AdessibindenFrontend.Helpers;
+using AdessibindenFrontend.Services.Abstract;
+using AdessibindenFrontend.Services.Results;
+using Application.Features.Auth.Commands.RefreshToken;
 using Blazored.LocalStorage;
 using Core.Security.JWT;
+using Core.Utilities.Results;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Newtonsoft.Json;
+using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Security.Claims;
 
 namespace AdessibindenFrontend.Services
@@ -13,26 +19,47 @@ namespace AdessibindenFrontend.Services
     {
         private readonly HttpClient _httpClient;
         private readonly ILocalStorageService _localStorage;
-        private readonly NavigationManager _navigationManager;
+        private readonly IAuthService _authService;
+        private readonly ILocalStorageService _localStorageService;
 
-        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage, NavigationManager navigationManager)
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage, IAuthService authService, ILocalStorageService localStorageService)
         {
             _httpClient = httpClient;
             _localStorage = localStorage;
-            _navigationManager = navigationManager;
+            _authService = authService;
+            _localStorageService = localStorageService;
         }
 
-                //TODO: refresh token
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        //TODO: fix refresh token auto login problem when logout
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync() //refactor needed
         {
             var token = await _localStorage.GetItemAsync<string>("local_token");
-            if (token == null)
-            {
-                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+            if (token != null) {
+                var accessToken = JsonConvert.DeserializeObject<AccessToken>(token);
+
+                if (!CheckIsTokenExpired(accessToken))
+                {
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken.Token);
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+                }
             }
-            var accessToken = JsonConvert.DeserializeObject<AccessToken>(token);
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", accessToken.Token);
-            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+
+            var refreshToken = await _authService.RefreshToken();
+            if (refreshToken.Success)
+                {
+                    await _localStorageService.SetItemAsync("local_token", refreshToken.Data.AccessToken);
+                    _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", refreshToken.Data.AccessToken.Token);
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(refreshToken.Data.AccessToken.Token), "jwtAuthType")));
+                }
+                
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
         }
+        
+        private bool CheckIsTokenExpired(AccessToken accessToken)
+        {
+            return accessToken.Expiration < DateTime.UtcNow;
+        }
+
     }
 }
