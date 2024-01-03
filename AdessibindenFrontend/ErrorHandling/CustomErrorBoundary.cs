ff5d51fa671f4cec18b7746f384cec6b892429fb
Emopusta <emreduman225@hotmail.com>
2024-01-03 10:35:55 +0300

    added custom error boundary to remove unnecessary console output

---
+++ AdessibindenFrontend/ErrorHandling/CustomErrorBoundary.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace AdessibindenFrontend.ErrorHandling
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        protected override Task OnErrorAsync(Exception exception)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
